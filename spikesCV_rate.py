3dd9805ddb1d687a6ec0f39b0e759cc2edd1ee45
murat.orynbayev@parisdescartes.fr <murat.orynbayev@parisdescartes.fr>
2020-11-30 10:59:49 +0100

    murat's new CV spike analysis

--- spikesCV_rate.py
+++ spikesCV_rate.py
@@ -13,13 +13,14 @@ from matplotlib.offsetbox import AnchoredText
 # filepath, file_start, file_end,
 # filter_pandas_series - filter order,
 # Histogram with bins
+# also without_first_N_samples = df.iloc[360:] deleting artefacts in the beginning of data
 #****************************************************************************
 filepath = '/home/murat/Documents/Data_Recorded_CNRS/2020_05_27/20200527_Cell407_001.smr'
 artifact_threshold = 0.2
 spike_threshold = 3
 #    range of analysing data
-file_start = 232  # seconds
-file_end = 253  # seconds
+file_start = 235  # seconds
+file_end = 237  # seconds
 hide_stim_artifacts = True
 spikelets_thresh = 0.5
 #******************************************************************************
@@ -69,34 +70,40 @@ vm, channels = get_spike_2(filepath)
 vm_filt = filter_pandas_series(vm, 100, type='high')
 
 #       converting file to .csv format
-#    ------------------------------------
+#    ----------------------------------------------------------------------
+#plt.plot(vm_filt)
 df = pd.DataFrame(vm_filt)
-# saving the dataframe as .csv
-df.to_csv('/home/murat/Desktop/file1.csv')
+without_first_N_samples = df.iloc[220:]  # deleting first N samples in dataframe because of artefact in the beginning
+#plt.plot(without_first_N_samples)
 
+# saving the dataframe as .csv
+without_first_N_samples.to_csv('/home/murat/Desktop/file1.csv')
 with open('/home/murat/Desktop/file1.csv') as file:
     reader = csv.reader(file, delimiter=',')
-signals = pd.read_csv('/home/murat/Desktop/file1.csv')
-#    -------------------------------------
+sig = pd.read_csv('/home/murat/Desktop/file1.csv')
+#    ---------------------------------------------------------------------
 
 #  creating data table and finding peaks from it
-signals = pd.DataFrame(signals)
+
+signals = pd.DataFrame(sig)
 signals.columns = ['time', 'Vm']
 #print(signals)
 
-peaks, _ = find_peaks(signals['Vm'], height=(0, 7), prominence=4)
+peaks, _ = find_peaks(signals['Vm'], height=(0, 7), prominence=7)
 peaks_table = pd.DataFrame(peaks)
 width_half = peak_widths(signals['Vm'], peaks, rel_height=0.25)
-#  for wdh dataframe [0]-row is half width; [1]-row is level of the half width; [2]-?; [3]-?
+
+#       for wdh dataframe [0]-row is half width; [1]-row is level of the half width; [2]-?; [3]-?
 wdh = pd.DataFrame(width_half)
 width_half[0]
 width_full = peak_widths(signals['Vm'], peaks, rel_height=1)
 width_full[0]
 # plt.plot(signals['Vm'])
-# plt.plot(peaks, signals['Vm'][peaks], "*" )
+# plt.plot(peaks, signals['Vm'][peaks], "*")
 # plt.hlines(*width_half[1:], color="C2")
 # plt.hlines(*width_full[1:], color="C3")
-#plt.show()
+# plt.show()
+
 
 # creating table of spikes with corresponding time, amplitude, samples
 rows = []
@@ -108,11 +115,14 @@ number_of_ISI = dh.index.max() - 1
 firing_rate = number_of_ISI / (file_end - file_start)
 print('firing_rate =', firing_rate, 'Hz')
 
+
+
 #  ------------------------------------------------------------------------------
 # calculating interspikes intervals (ISI) and coefficient of variance (CV),
 intervals = []
 for i in range(0, dh.index.max()):
     intervals.append([i, dh._get_value(index=i+1, col='time') - dh._get_value(index=i, col='time')])
+
 ISI_table = pd.DataFrame(intervals, columns=["N", "ISI"])
 spike_intervals = np.array(intervals)[:, 1]
 mean_of_ISI = np.mean(spike_intervals)
@@ -123,20 +133,27 @@ print('coefficient of variance =', coef_var)
 
 #  --------------------------------------------------------------------------------
 
-#  Histogram with bins
-plt.hist(spike_intervals, density=True, bins=80)   # 'density=False' would make counts
-mn, mx = plt.xlim()
-plt.xlim(mn, mx)
-kde_xs = np.linspace(mn, mx, 50)
-kde = st.gaussian_kde(spike_intervals)
-kde_xs = np.linspace(mn, mx, number_of_ISI+1)
-# plt.ylabel('Counts')
-# plt.xlabel('Intervals')
+# calculating spike widths at half peak; creating table of spikes_widths, with corresponding samples
+wdh_0 = np.array(width_half[0])
+widathalf = pd.DataFrame(wdh_0, columns=["width"])
+widths_rows = []
+for i in range(0, widathalf.index.max()):
+    widths_rows.append([i, widathalf._get_value(index=i, col='width')])
 
-x = np.linspace(-0.02, 0.1, 1000)
-y = st.norm(4.0, 2.0).pdf(x)
+spikes_widths = np.array(widths_rows)[:, 1]
+number_of_all_widths = widathalf.index.max()
+mean_of_spikes_widths = np.mean(spikes_widths)
+stdev_widths = np.std(spikes_widths, ddof=1)  # standard deviation (SD) of intervals;  ddof=1 for case 1/(N-1),  ddof=0 for case 1/N
+coef_var_width = stdev_widths/mean_of_spikes_widths
+sterr_widths = stdev_widths / np.sqrt(spikes_widths.size)  #  standard error of the mean (SEM)
 
-fig, axes = plt.subplots(2, 1, figsize=(12, 9))
+
+# x = np.linspace(-0.02, 0.1, 1000)
+# y = st.norm(4.0, 2.0).pdf(x)
+#
+
+#   making Figures
+fig, axes = plt.subplots(3, 1, figsize=(10, 6))
 axes[0].plot(signals['Vm'])
 axes[0].plot(peaks, signals['Vm'][peaks], "*")
 axes[0].set_xlabel("samples")
@@ -144,25 +161,45 @@ axes[0].set_ylabel("voltage (mV)")
 axes[0].hlines(*width_half[1:], color="C2")  # zoom single spike, you will see half width line
 #axes[0].hlines(*width_full[1:], color="C3")
 
-# axes[1].plot(x, y, 'r', lw=2)
-# axes[1].set_xlim(-0.02, 0.1)
+mn, mx = plt.xlim(left=spike_intervals.min(), right=spike_intervals.max())
+kde_xs = np.linspace(mn, mx, number_of_ISI+1)
+kde = st.gaussian_kde(spike_intervals)
 axes[1].hist(spike_intervals, density=True, bins=80)
 axes[1].mn, mx = plt.xlim()
 axes[1].set_xlim(mn, mx)
-axes[1].kde_xs = np.linspace(mn, mx, 1000)
+axes[1].kde_xs = np.linspace(mn, mx, number_of_ISI+1)
 axes[1].kde = st.gaussian_kde(spike_intervals)
 axes[1].plot(kde_xs, kde.pdf(kde_xs), label="PDF")
 axes[1].set_xlabel("intervals (s)")
 axes[1].set_ylabel("counts")
 
-#         anotation in the figure
+#         anotation in the figure #2
 anotation = "CV = " + str(np.round(coef_var, 5))
 anotation += "\n"
 anotation += "firing rate = " + str(np.round(firing_rate, 5))
 anotation += "\n"
 anotation += "ISI mean = " + str(np.round(mean_of_ISI, 5)) + '\xB1' + str(np.round(stdev_intervals, 5))
-at = AnchoredText(anotation, prop=dict(size=10), frameon=True, loc='upper left')
+at = AnchoredText(anotation, prop=dict(size=10), frameon=True, loc='upper right')
 at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
 axes[1].add_artist(at)
 
-plt.show()
+
+mi, ma = plt.xlim(left=spikes_widths.min(), right=spikes_widths.max())
+kde_wh = np.linspace(mi, ma, number_of_all_widths)
+gauss_kde = st.gaussian_kde(spikes_widths)
+axes[2].hist(spikes_widths, density=True, bins=80)
+axes[2].mi, ma = plt.xlim(left=spikes_widths.min(), right=spikes_widths.max())
+axes[2].set_xlim(mi, ma)
+axes[2].kde_wh = np.linspace(mi, ma, number_of_all_widths)
+axes[2].gauss_kde = st.gaussian_kde(spikes_widths)
+axes[2].plot(kde_wh, gauss_kde.pdf(kde_wh), label="PDF")
+axes[2].set_xlabel("spike width (s)")
+axes[2].set_ylabel("counts")
+
+#         anotation in the figure #3
+anotation = "CV = " + str(np.round(coef_var_width, 5))
+anotation += "\n"
+anotation += "widths mean = " + str(np.round(mean_of_spikes_widths, 5)) + '\xB1' + str(np.round(stdev_widths, 5))
+at = AnchoredText(anotation, prop=dict(size=10), frameon=True, loc='upper right')
+at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+axes[2].add_artist(at)
